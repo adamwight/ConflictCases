dafd23bd44536130e3d4dff23239530360375b7e
Adam Wight <adam.wight@wikimedia.de>
2019-08-16 16:14:02 +0200

    Stub out the main hook
    Change-Id: Ia22132bcfc093176fef42784181d99de51a989f7

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,32 +1,26 @@
 <?php
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * @file
- */
 
-namespace MediaWiki\Extension\BoilerPlate;
+namespace MediaWiki\Extension\ConflictCases;
+
+use Content;
+use Title;
 
 class Hooks {
 
 	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/Something
-	 * @param string $arg1 First argument
-	 * @param array $arg2
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/EditPage::threeWayMerge:after
+	 * @param Title $title Title of the page being edited
+	 * @param Content $baseContent Content of the page before this edit
+	 * @param Content $editContent Content after this edit
+	 * @param Content $currentContent Content with conflicting edits
+	 * @param Content &$result Content after built-in 3-way merge. Can be
+	 *        modified by the hook: if it is false-ish, then the edit is an
+	 *        unresolved conflict.  If content is provided, it will be saved as
+	 *        the new page content.
 	 */
-	public static function onSomething( $arg1, array $arg2 ) {
+	public static function onMergeAfter( Title $title, Content $baseContent,
+		Content $editContent, Content $currentContent, Content &$result
+	) {
 		// Stub
 	}
 
